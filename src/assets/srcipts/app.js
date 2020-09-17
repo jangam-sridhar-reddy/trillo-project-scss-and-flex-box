9e6e7555d0b7445779317b920f4ab1fcca46507e
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-17 16:37:02 +0530

    header completed.

--- src/assets/srcipts/app.js
+++ src/assets/srcipts/app.js
@@ -1,2 +1,3 @@
 import "@babel/polyfill";
 import "../sass/main.scss";
+import '../img/sprite.svg';
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 module.exports = {
     entry : "./src/assets/srcipts/app.js",
@@ -10,7 +10,7 @@ module.exports = {
                 loader: 'html-loader',
             },
             {
-                test : /\.(png|jpg|jpeg|svg|gif)$/,
+                test : /\.(png|jpg|jpeg|gif)$/,
                 use : {
                     loader : "file-loader",
                     options : {
@@ -28,12 +28,25 @@ module.exports = {
                     loader: "babel-loader"
                 }
             
+            },
+            {
+                test : /\.svg/i,
+                include: /.*sprite\.svg/,
+                use: [
+                    {
+                        loader: 'svg-sprite-loader',
+                        options: {
+                            publicPath: '',
+                        }
+                    },
+                ],
             }
         ]
     },
     plugins : [
         new HtmlWebpackPlugin(
             {template : "./src/index.html"}
-        )
+        ),
+        new SpriteLoaderPlugin()
     ]
 }
